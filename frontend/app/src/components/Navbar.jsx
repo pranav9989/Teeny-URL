0016572250fec21e0c3ec59a2ec0bc9e9b60357b
pranav9989 <pranavraghuvanshi2924@gmail.com>
2026-07-23 15:49:58 +0530

    Navbar created

---
+++ frontend/app/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <a href="/">HOME</a>
+                <a href="/stats">STATS</a>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
